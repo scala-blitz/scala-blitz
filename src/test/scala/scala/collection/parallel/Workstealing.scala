866a13f51ed3a3ae73359343ea74dd00640f7fd9
Aleksandar Prokopec <axel22@gmail.com>
2012-08-29 21:22:27 +0200

    WIP got workstealing to work.

--- src/test/scala/scala/collection/parallel/Workstealing.scala
+++ src/test/scala/scala/collection/parallel/Workstealing.scala
@@ -4,6 +4,7 @@ package scala.collection.parallel
 
 import sun.misc.Unsafe
 import annotation.{tailrec, static}
+import collection._
 
 
 
@@ -90,11 +91,103 @@ object AdvLoop extends testing.Benchmark {
 }
 
 
+object EqualLoadLoop extends testing.Benchmark {
+
+  val size = sys.props("size").toInt
+  val par = sys.props("par").toInt
+
+  def quickloop(start: Int, limit: Int) = {
+    var i = start
+    var sum = 0
+    while (i < limit) {
+      sum += i
+      i += 1
+    }
+    sum
+  }
+
+  final class Worker extends Thread {
+    @volatile var result = 0
+    override def run() {
+      var i = 0
+      val until = size / par
+      result = quickloop(0, until)
+    }
+  }
+
+  def run() {
+    val workers = for (_ <- 0 until par) yield new Worker
+    for (w <- workers) w.start()
+    for (w <- workers) w.join()
+    workers.map(_.result).sum
+  }
+
+}
+
+
 object StealLoop extends testing.Benchmark {
 
-  final class Ptr(@volatile var child: Node) {
+  final class Ptr(val level: Int)(@volatile var child: Node) {
     def casChild(ov: Node, nv: Node) = unsafe.compareAndSwapObject(this, Ptr.CHILD_OFFSET, ov, nv)
     def writeChild(nv: Node) = unsafe.putObjectVolatile(this, Ptr.CHILD_OFFSET, nv)
+
+    /** Try to expand node and return true if node was expanded.
+     *  Return false if node was completed.
+     */
+    @tailrec def expand(): Boolean = {
+      val child_t0 = /*READ*/child
+      if (!child_t0.isLeaf) true else { // already expanded
+        // first set progress to -progress
+        val progress_t1 = /*READ*/child_t0.progress
+        if (progress_t1 == child_t0.until) false // already completed
+        else {
+          if (progress_t1 >= 0) {
+            if (child_t0.casProgress(progress_t1, -(progress_t1 + 1))) expand() // marked stolen - now move on to node creation
+            else expand() // wasn't marked stolen and failed marking stolen - retry
+          } else { // marked stolen
+            // node effectively immutable (except for `result`) - expand it
+            val expanded = child_t0.newExpanded
+            if (casChild(child_t0, expanded)) true // try to replace with expansion
+            else expand() // failure (spurious or due to another expand) - retry
+          }
+        }
+      }
+    }
+
+    def toString(l: Int): String = "\t" * level + "Ptr" + level + " -> " + child.toString(l)
+
+    def balance: collection.immutable.HashMap[Owner, Int] = {
+      val here = collection.immutable.HashMap(child.owner -> (child.progress - child.start))
+      if (child.isLeaf) here
+      else Seq(here, child.left.balance, child.right.balance).foldLeft(collection.immutable.HashMap[Owner, Int]()) {
+        case (a, b) => a.merged(b) {
+          case ((ak, av), (bk, bv)) => ((ak, av + bv))
+        }
+      }
+    }
+
+    /** Whether stealer should go left at this level. */
+    def chooseAsStealer(index: Int, total: Int): Boolean = {
+      if (isTreeTop(total, level)) chooseInTreeTop(index, level)
+      else false
+    }
+
+    /** Whether victim should go left at this level. */
+    def chooseAsVictim(index: Int, total: Int): Boolean = {
+      if (isTreeTop(total, level)) chooseInTreeTop(index, level)
+      else true
+    }
+
+    def choosePtrAsStealer(index: Int, total: Int): Ptr = {
+      if (chooseAsStealer(index, total)) child.left
+      else child.right
+    }
+
+    def choosePtrAsVictim(index: Int, total: Int): Ptr = {
+      if (chooseAsVictim(index, total)) child.left
+      else child.right
+    }
+
   }
 
   object Ptr {
@@ -107,7 +200,7 @@ object StealLoop extends testing.Benchmark {
   final class Node(val left: Ptr, val right: Ptr, val parent: Ptr)(val start: Int, @volatile var progress: Int, val step: Int, val until: Int) {
     /*
      * progress: start -> x1 -> ... -> xn -> until
-     *                                    -> -xn
+     *                                    -> -(xn + 1)
      * x1 > start, x(i+1) > xi, until > xn
      * 
      * owner: null -> v, v != null
@@ -120,24 +213,6 @@ object StealLoop extends testing.Benchmark {
 
     final def casProgress(ov: Int, nv: Int) = unsafe.compareAndSwapInt(this, Node.PROGRESS_OFFSET, ov, nv)
     final def casOwner(ov: Owner, nv: Owner) = unsafe.compareAndSwapObject(this, Node.OWNER_OFFSET, ov, nv)
-
-    /** Try to expand node and return true if node was expanded.
-     *  Return false if node was completed.
-     */
-    @tailrec def expand(): Boolean = if (parent.child != this) true else { // already expanded
-      // first set progress to -progress
-      val progress_t0 = /*READ*/progress
-      if (progress_t0 == until) false // already completed
-      else {
-        if (progress_t0 >= 0 && !casProgress(progress_t0, -progress_t0)) expand() // not marked stolen and failed marking stolen
-        else { // marked stolen
-          // node effectively immutable (except for `result`) - expand it
-          val expanded = createExpanded()
-          if (parent.casChild(this, expanded)) true // try to replace with expansion
-          else expand() // failure (spurious or due to another expand) - retry
-        }
-      }
-    }
 
     def nonEmpty = (until - start) > 0
 
@@ -154,21 +229,29 @@ object StealLoop extends testing.Benchmark {
       else tryOwn(thiz)
     }
 
-    def trySteal(): Boolean = expand()
+    def trySteal(): Boolean = parent.expand()
 
-    def createExpanded() = {
-      val p = /*READ*/progress
+    def newExpanded: Node = {
+      val stolenP = /*READ*/progress
+      val p = -(stolenP) - 1
       val remaining = until - p
       val firsthalf = remaining / 2
       val secondhalf = remaining - firsthalf
-      val lptr = new Ptr(null)
-      val rptr = new Ptr(null)
+      val lptr = new Ptr(parent.level + 1)(null)
+      val rptr = new Ptr(parent.level + 1)(null)
       val lnode = new Node(null, null, lptr)(p, p, step, p + firsthalf)
       val rnode = new Node(null, null, rptr)(p + firsthalf, p + firsthalf, step, until)
       lptr.writeChild(lnode)
       rptr.writeChild(rnode)
       val nnode = new Node(lptr, rptr, parent)(start, p, step, until)
+      nnode.owner = this.owner
       nnode
+    }
+
+    def toString(lev: Int): String = {
+      "Node(%s: %d, %d, %d, %d)".format(owner, start, progress, step, until) + (if (!isLeaf) {
+        "\n" + left.toString(lev + 1) + right.toString(lev + 1)
+      } else "\n")
     }
   }
 
@@ -182,6 +265,23 @@ object StealLoop extends testing.Benchmark {
   val size = sys.props("size").toInt
   val block = sys.props("block").toInt
   val par = sys.props("par").toInt
+  val inspectgc = sys.props.getOrElse("inspectgc", "false").toBoolean
+
+  /** Returns true iff the level of the tree is such that: 2^level < total */
+  final def isTreeTop(total: Int, level: Int): Boolean = (1 << level) < total
+
+  /** Returns true iff the worker should first go left at this level of the tree top. */
+  final def chooseInTreeTop(index: Int, level: Int): Boolean = ((index >> level) & 0x1) == 0
+
+  /** Returns true if the worker labeled with `index` with a total of
+   *  `total` workers should go left at level `level`.
+   *  Returns false otherwise.
+   */
+  final def choose(index: Int, total: Int, level: Int): Boolean = {
+    if (isTreeTop(total, level)) {
+      chooseInTreeTop(index, level)
+    } else random.nextBoolean
+  }
 
   private def quickloop(start: Int, limit: Int): Int = {
     var i = start
@@ -202,7 +302,7 @@ object StealLoop extends testing.Benchmark {
     var looping = true
     while (looping) {
       val currprog = /*READ*/work.progress
-      val nextprog = currprog + work.step
+      var nextprog = math.min(currprog + work.step, work.until)
       if (currprog < work.until && currprog >= 0) {
         if (work.casProgress(currprog, nextprog)) sum += quickloop(currprog, nextprog)
       } else looping = false
@@ -211,13 +311,16 @@ object StealLoop extends testing.Benchmark {
       work.result = Some(sum)
       true
     } else if (work.stolen) {
-      if (work.isLeaf) work.expand()
+      // help expansion if necessary
+      if (tree.child.isLeaf) tree.expand()
       tree.child.result = Some(sum)
       false
-    } else sys.error("unreachable")
+    } else sys.error("unreachable: " + work.progress + ", " + work.until)
   }
   
-  class Worker(val root: Ptr) extends Thread {
+  class Worker(val root: Ptr, val index: Int, val total: Int) extends Thread {
+    setName("Worker: " + index)
+
     def findWork(tree: Ptr): Ptr = {
       val node = tree.child
       if (node.isLeaf) {
@@ -225,12 +328,15 @@ object StealLoop extends testing.Benchmark {
         else {
           // more work
           if (node.tryOwn(this)) tree
-          else if (node.trySteal() && node.right.child.tryOwn(this)) node.right
-          else findWork(tree)
+          else if (node.trySteal()) {
+            val subnode = tree.choosePtrAsStealer(index, total)
+            if (subnode.child.tryOwn(this)) subnode
+            else findWork(tree)
+          } else findWork(tree)
         }
       } else {
         // descend deeper
-        if (random.nextBoolean) {
+        if (choose(index, total, root.level)) {
           val ln = findWork(node.left)
           if (ln != null) ln else findWork(node.right)
         } else {
@@ -243,13 +349,14 @@ object StealLoop extends testing.Benchmark {
     @tailrec override final def run() {
       val leaf = findWork(root)
       if (leaf != null) {
-        @tailrec def work(leaf: Ptr) {
+        @tailrec def workAndDescend(leaf: Ptr) {
           val nosteals = workloop(leaf)
           if (!nosteals) {
-            if (leaf.child.left.child.tryOwn(this)) work(leaf.child.left)
+            val subnode = leaf.choosePtrAsVictim(index, total)
+            if (subnode.child.tryOwn(this)) workAndDescend(subnode)
           }
         }
-        work(leaf)
+        workAndDescend(leaf)
         run()
       } else {
         // no more work
@@ -257,12 +364,39 @@ object StealLoop extends testing.Benchmark {
     }
   }
 
+  var repetition = 0
+  var root: Ptr = _
+
   def run() {
-    val root = new Ptr(null)
+    root = new Ptr(0)(null)
     val work = new Node(null, null, root)(0, 0, block, size)
     root.writeChild(work)
 
-    workloop(root)
+    val workers = for (i <- 0 until par) yield new Worker(root, i, par)
+    for (w <- workers) w.start()
+    for (w <- workers) w.join()
+  }
+
+  override def setUp() {
+    if (inspectgc) {
+      println("run starting...")
+      Thread.sleep(500)
+    }
+  }
+
+  override def tearDown() {
+    if (inspectgc) {
+      Thread.sleep(500)
+      println("run completed...")
+    }
+    repetition += 1
+    if (repetition == 15) {
+      val balance = root.balance
+      println(root.toString(0))
+      println(balance.toList.sortBy(_._1.getName).map(p => p._1 + ": " + p._2).mkString("work balance:\n", "\n", ""))
+      println("total: " + balance.foldLeft(0)(_ + _._2))
+    }
+    root = null
   }
 
 }
