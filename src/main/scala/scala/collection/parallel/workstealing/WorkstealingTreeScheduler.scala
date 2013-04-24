37f271cecb9173dcaa60a3dbe21c6c89fd0a3692
Aleksandar Prokopec <axel22@gmail.com>
2013-04-24 10:58:01 +0200

    Refactoring kernel default node handling.

--- src/main/scala/scala/collection/parallel/workstealing/WorkstealingTreeScheduler.scala
+++ src/main/scala/scala/collection/parallel/workstealing/WorkstealingTreeScheduler.scala
@@ -10,13 +10,13 @@ import scala.annotation.tailrec
 abstract class WorkstealingTreeScheduler {
   import WorkstealingTreeScheduler._
 
-  def config: Config
-
   val invoker = new Worker {
     def index = 0
-    def total = config.par
-    def getName = "Invoker"
+    def total = config.parallelismLevel
+    def name = "Invoker"
   }
+
+  def config: Config
 
   def dispatchWork[T, R](root: Ref[T, R], kernel: Kernel[T, R]): Unit
 
@@ -74,20 +74,48 @@ object WorkstealingTreeScheduler {
   val NO_RESULT = new AnyRef
   val INTERMEDIATE_READY = new AnyRef
 
+  trait Config {
+    def parallelismLevel: Int
+    def incrementStepFrequency: Int
+    def maximumStep: Int
+    def stealingStrategy: Strategy
+  }
+
   trait Worker {
     def index: Int
     def total: Int
-    def getName: String
+    def name: String
+  }
+
+  trait TerminationCause {
+    def validateResult[R](r: R): R
   }
 
   trait Kernel[@specialized T, R] {
+    /** Used for cancelling operations early (e.g. due to exceptions).
+     *  Holds information on why the operation failed
+     */
+    @volatile protected var terminationCause: TerminationCause = null
+
+    /** Returns `true` as long as `terminationCause` is `null`.
+     */
+    def notTerminated = terminationCause eq null
+
+    /** Returns the result if there was no early termination.
+     *  Otherwise may throw an exception based on the termination cause.
+     */
+    def validateResult(r: R): R = {
+      if (notTerminated) r
+      else terminationCause.validateResult(r)
+    }
+
     /** Initializes the workstealing tree node.
      *
      *  By default does nothing, but some kernels may choose to override this default behaviour
      *  to store operation-specific information into the node.
      */
     def beforeWorkOn(tree: Ref[T, R], node: Node[T, R]) {
-      node.intermediateResult = zero.asInstanceOf[AnyRef]
+      node.WRITE_INTERMEDIATE(zero)
     }
 
     /** Initializes the workstealing tree root.
@@ -108,8 +136,120 @@ object WorkstealingTreeScheduler {
      *  This behaviour can be overridden.
      */
     def storeIntermediateResult(node: Node[T, R], res: R) {
-      node.intermediateResult = res.asInstanceOf[AnyRef]
+      node.WRITE_INTERMEDIATE(res)
     }
+
+    /** Returns true if completed with no stealing.
+     *  Returns false if steal occurred.
+     *
+     *  May be overridden in subclass to specialize for better performance.
+     */
+    def workOn(tree: Ref[T, R], config: Config, worker: Worker): Boolean = {
+      import Stealer._
+
+      // atomically read the current node and initialize
+      val node = tree.READ
+      val stealer = node.stealer
+      beforeWorkOn(tree, node)
+      var intermediate = node.READ_INTERMEDIATE
+      var incCount = 0
+      val incFreq = config.incrementStepFrequency
+      val ms = config.maximumStep
+
+      // commit to processing chunks of the collection and process them until termination
+      var looping = true
+      while (looping && notTerminated) {
+        val currstep = node.READ_STEP
+        val currstate = stealer.state
+  
+        if (currstate != Completed && currstate != StolenOrExpanded) {
+          // reserve some work
+          val chunk = stealer.advance(currstep)
+
+          if (chunk != -1) intermediate = combine(intermediate, apply(node, chunk))
+  
+          // update step
+          incCount = (incCount + 1) % incFreq
+          if (incCount == 0) node.WRITE_STEP(math.min(ms, currstep * 2))
+        } else looping = false
+      }
+
+      completeIteration(node.stealer)
+
+      // store into the `intermediateResult` field of the node and push result up
+      completeNode(intermediate, tree, worker)
+    }
+
+    /** Completes the iteration in the stealer.
+     * 
+     *  Some parallel operations do not traverse all the elements in a chunk or a node. The purpose of this
+     *  method is to bring the node into a Completed or Stolen state before proceeding.
+     */
+    protected final def completeIteration(stealer: Stealer[T]) {
+      var state = stealer.state
+      while (state eq Stealer.AvailableOrOwned) {
+        stealer.markCompleted()
+        state = stealer.state
+      }
+    }
+
+    /** Completes the iteration in the node.
+     * 
+     *  Some parallel operations do not traverse all the elements in a chunk or a node. The purpose of this
+     *  method is to bring the node into a Completed or Stolen state before proceeding.
+     */
+    private def completeNode(intermediate: R, tree: Ref[T, R], worker: Worker): Boolean = {
+      import Stealer._
+      val node_t0 = tree.READ
+      val state_t1 = node_t0.stealer.state
+      val wasCompleted = if (state_t1 eq Completed) {
+        storeIntermediateResult(node_t0, intermediate)
+        while (node_t0.READ_RESULT == NO_RESULT) node_t0.CAS_RESULT(NO_RESULT, INTERMEDIATE_READY)
+        true
+      } else if (state_t1 eq StolenOrExpanded) {
+        // help expansion if necessary
+        if (tree.READ.isLeaf) tree.markExpand(this, worker)
+
+        val node_t2 = tree.READ
+        storeIntermediateResult(node_t2, intermediate)
+        while (node_t2.result == NO_RESULT) node_t2.CAS_RESULT(NO_RESULT, INTERMEDIATE_READY)
+        false
+      } else sys.error("unreachable: " + state_t1 + ", " + node_t0.toString(0))
+  
+      // push result up as far as possible
+      pushUp(tree)
+  
+      wasCompleted
+    }
+    
+    @tailrec private def pushUp(tree: Ref[T, R]) {
+      val node_t0 = tree.READ
+      val r_t1 = /*READ*/node_t0.result
+      r_t1 match {
+        case NO_RESULT =>
+          // we're done, owner did not finish his work yet
+        case INTERMEDIATE_READY =>
+          if (node_t0.isLeaf) node_t0.READ_INTERMEDIATE
+          else {
+            // check if result already set for children
+            def available(r: AnyRef) = r != NO_RESULT && r != INTERMEDIATE_READY
+            val lr = node_t0.left.READ.READ_RESULT
+            val rr = node_t0.right.READ.READ_RESULT
+            if (available(lr) && available(rr)) {
+              val subr = combine(lr.asInstanceOf[R], rr.asInstanceOf[R])
+              val r = combine(node_t0.READ_INTERMEDIATE, subr).asInstanceOf[AnyRef]
+              if (node_t0.CAS_RESULT(r_t1, r)) {
+                // if at root, notify completion, otherwise go one level up
+                if (tree.up == null) tree.synchronized {
+                  tree.notifyAll()
+                } else pushUp(tree.up)
+              } else pushUp(tree) // retry
+            }
+          }
+        case _ =>
+          // we're done, somebody else already pushed up
+      }
+    }    
 
     /** The neutral element of the reduction.
      */
@@ -162,7 +302,7 @@ object WorkstealingTreeScheduler {
   final class Node[@specialized T, R](val left: Ref[T, R], val right: Ref[T, R])(val stealer: Stealer[T]) {
     @volatile var step: Int = 1
     @volatile var owner: Worker = null
-    @volatile var intermediateResult: AnyRef = null
+    @volatile var intermediateResult: R = _
     @volatile var result: AnyRef = NO_RESULT
 
     def CAS_OWNER(ov: Worker, nv: Worker) = unsafe.compareAndSwapObject(this, OWNER_OFFSET, ov, nv)
@@ -172,6 +312,12 @@ object WorkstealingTreeScheduler {
     def CAS_RESULT(ov: AnyRef, nv: AnyRef) = unsafe.compareAndSwapObject(this, RESULT_OFFSET, ov, nv)
     def WRITE_RESULT(nv: AnyRef) = unsafe.putObjectVolatile(this, RESULT_OFFSET, nv)
     def READ_RESULT = unsafe.getObjectVolatile(this, RESULT_OFFSET).asInstanceOf[AnyRef]
+
+    def READ_INTERMEDIATE = intermediateResult
+    def WRITE_INTERMEDIATE(v: R) = intermediateResult = v
+
+    def READ_STEP = step
+    def WRITE_STEP(v: Int) = step = v
 
     final def isLeaf = left eq null
 
