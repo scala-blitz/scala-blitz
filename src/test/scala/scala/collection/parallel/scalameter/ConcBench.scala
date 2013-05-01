c38a81b11af3cbf3f95f78502538773c5b3458f2
Aleksandar Prokopec <axel22@gmail.com>
2013-05-01 10:17:12 +0200

    Uncommenting benchmarks.

--- src/test/scala/scala/collection/parallel/scalameter/ConcBench.scala
+++ src/test/scala/scala/collection/parallel/scalameter/ConcBench.scala
@@ -19,7 +19,7 @@ class ConcBench extends PerformanceTest.Regression with Serializable {
   /* generators */
 
   val sizes = Gen.enumeration("size")(500000, 1000000, 1500000)
-  val appendconcs = for (size <- sizes) yield {
+  val concs = for (size <- sizes) yield {
     var conc: Conc[Int] = Conc.Zero
     for (i <- 0 until size) conc = conc <> i
     conc
@@ -27,77 +27,77 @@ class ConcBench extends PerformanceTest.Regression with Serializable {
 
   performance of "Conc" in {
 
-    // measure method "<>(T)" config(
-    //   exec.benchRuns -> 35,
-    //   exec.independentSamples -> 5,
-    //   exec.jvmflags -> "-XX:+UseCondCardMark"
-    // ) in {
-    //   using(sizes) curve("List") in { sz =>
-    //     var list: List[Unit] = Nil
-    //     var i = 0
-    //     while (i < sz) {
-    //       list ::= ()
-    //       i += 1
-    //     }
-    //   }
+    measure method "<>(T)" config(
+      exec.benchRuns -> 35,
+      exec.independentSamples -> 5,
+      exec.jvmflags -> "-XX:+UseCondCardMark"
+    ) in {
+      using(sizes) curve("List") in { sz =>
+        var list: List[Unit] = Nil
+        var i = 0
+        while (i < sz) {
+          list ::= ()
+          i += 1
+        }
+      }
 
-    //   using(sizes) curve("Vector") in { sz =>
-    //     var v = collection.immutable.Vector[Unit]()
-    //     var i = 0
-    //     while (i < sz) {
-    //       v = v :+ ()
-    //       i += 1
-    //     }
-    //   }
+      using(sizes) curve("Vector") in { sz =>
+        var v = collection.immutable.Vector[Unit]()
+        var i = 0
+        while (i < sz) {
+          v = v :+ ()
+          i += 1
+        }
+      }
 
-    //   using(sizes) curve("Conc") in { sz =>
-    //     import Conc._
-    //     var conc: Conc[Int] = Zero
-    //     var i = 0
-    //     while (i < sz) {
-    //       conc = conc <> i
-    //       i += 1
-    //     }
-    //   }
-    // }
+      using(sizes) curve("Conc") in { sz =>
+        import Conc._
+        var conc: Conc[Int] = Zero
+        var i = 0
+        while (i < sz) {
+          conc = conc <> i
+          i += 1
+        }
+      }
+    }
 
-    // performance of "Buffer" config(
-    //   exec.benchRuns -> 35,
-    //   exec.independentSamples -> 5,
-    //   exec.jvmflags -> "-XX:+UseCondCardMark"
-    // ) in {
-    //   using(sizes) curve("VectorBuffer") in { sz =>
-    //     val vb = new collection.immutable.VectorBuilder[Unit]()
-    //     var i = 0
-    //     while (i < sz * 2) {
-    //       vb += ()
-    //       i += 1
-    //     }
-    //   }
+    performance of "Buffer" config(
+      exec.benchRuns -> 35,
+      exec.independentSamples -> 5,
+      exec.jvmflags -> "-XX:+UseCondCardMark"
+    ) in {
+      using(sizes) curve("VectorBuffer") in { sz =>
+        val vb = new collection.immutable.VectorBuilder[Unit]()
+        var i = 0
+        while (i < sz * 2) {
+          vb += ()
+          i += 1
+        }
+      }
 
-    //   using(sizes) curve("Conc.Buffer") in { sz =>
-    //     val cb = new Conc.Buffer[Int]
-    //     var i = 0
-    //     while (i < sz * 2) {
-    //       cb += i
-    //       i += 1
-    //     }
-    //   }
-    // }
+      using(sizes) curve("Conc.Buffer") in { sz =>
+        val cb = new Conc.Buffer[Int]
+        var i = 0
+        while (i < sz * 2) {
+          cb += i
+          i += 1
+        }
+      }
+    }
 
-    // measure method "<>(Conc)" config(
-    //   exec.benchRuns -> 35,
-    //   exec.independentSamples -> 5,
-    //   exec.jvmflags -> "-XX:+UseCondCardMark"
-    // ) in {
-    //   using(appendconcs) curve("Conc") in { conc =>
-    //     var i = 0
-    //     while (i < 10000) {
-    //       conc <> conc
-    //       i += 1
-    //     }
-    //   }
-    // }
+    measure method "<>(Conc)" config(
+      exec.benchRuns -> 35,
+      exec.independentSamples -> 5,
+      exec.jvmflags -> "-XX:+UseCondCardMark"
+    ) in {
+      using(concs) curve("Conc") in { conc =>
+        var i = 0
+        while (i < 10000) {
+          conc <> conc
+          i += 1
+        }
+      }
+    }
 
     performance of "append-normalize-merge" config(
       exec.benchRuns -> 35,
