6b5e5afd9124826216c1d2cd69303e27951013e4
Aleksandar Prokopec <axel22@gmail.com>
2013-06-11 13:23:21 +0200

    Hash stealer tests.

--- src/main/scala/scala/collection/parallel/workstealing/Hashes.scala
+++ src/main/scala/scala/collection/parallel/workstealing/Hashes.scala
@@ -7,6 +7,7 @@ import scala.language.experimental.macros
 import scala.reflect.macros._
 import scala.reflect.ClassTag
 import scala.collection.parallel.generic._
+import scala.collection.mutable.HashTable
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.HashEntry
 import scala.collection.mutable.DefaultEntry
@@ -28,15 +29,20 @@ object Hashes {
     }
   }
 
-  class HashMapOps[K, V](val array: Par[HashMap[K, V]]) extends AnyVal with Reducables.OpsLike[(K, V), Par[HashMap[K, V]]] {
-    def stealer: Stealer[(K, V)] = ???
+  class HashMapOps[K, V](val hashmap: Par[HashMap[K, V]]) extends AnyVal with Reducables.OpsLike[(K, V), Par[HashMap[K, V]]] {
+    def stealer: Stealer[(K, V)] = {
+      val contents = hashmap.seq.hashTableContents
+      new HashMapStealer(contents, 0, contents.table.length)
+    }
     def aggregate[S](z: S)(combop: (S, S) => S)(seqop: (S, (K, V)) => S)(implicit ctx: WorkstealingTreeScheduler) = ???
   }
 
   abstract class HashStealer[T](si: Int, ei: Int) extends IndexedStealer[T](si, ei) {
     type StealerType <: HashStealer[T]
 
-    def elementsRemainingEstimate = indicesRemaining
+    def elementsRemainingEstimate = (indicesRemaining.toLong * loadFactor / 1000).toInt
+
+    protected def loadFactor: Int
 
     protected def positionAt(idx: Int): Boolean
 
@@ -44,34 +50,32 @@ object Hashes {
 
     protected def nextAt: T
 
-    def advance() {
-      var i = nextProgress + 1
+    def moveForward() {
+      var i = nextProgress
       while (i < nextUntil) {
         if (positionAt(i)) {
-          nextProgress = i
+          nextProgress = i + 1
           i = nextUntil
         } else i += 1
       }
     }
 
     def hasNext: Boolean = if (hasNextAt) true else {
-      advance()
+      moveForward()
       hasNextAt
     }
 
-    def next(): T = nextAt
+    def next(): T = if (hasNext) nextAt else throw new NoSuchElementException
 
-    def split: (HashStealer[T], HashStealer[T]) = splitAtIndex(elementsRemainingEstimate / 2) 
-
-    advance()
+    def split: (HashStealer[T], HashStealer[T]) = splitAtIndex(indicesRemaining / 2)
   }
 
-  final class HashMapStealer[K, V](val table: Array[HashEntry[K, DefaultEntry[K, V]]], si: Int, ei: Int) extends HashStealer[(K, V)](si, ei) {
+  final class HashMapStealer[K, V](val contents: HashTable.Contents[K, DefaultEntry[K, V]], si: Int, ei: Int) extends HashStealer[(K, V)](si, ei) {
     type StealerType = HashMapStealer[K, V]
 
-    var entry: HashEntry[K, DefaultEntry[K, V]] = _
+    val table: Array[HashEntry[K, DefaultEntry[K, V]]] = contents.table
+    var entry: HashEntry[K, DefaultEntry[K, V]] = null
 
-    var padding9: Int = _
     var padding10: Int = _
     var padding11: Int = _
     var padding12: Int = _
@@ -79,7 +83,9 @@ object Hashes {
     var padding14: Int = _
     var padding15: Int = _
 
-    def newStealer(si: Int, ei: Int) = new HashMapStealer(table, si, ei)
+    def newStealer(si: Int, ei: Int) = new HashMapStealer(contents, si, ei)
+
+    protected def loadFactor: Int = contents.loadFactor
 
     protected def positionAt(idx: Int): Boolean = if (table(idx) == null) false else {
       entry = table(idx)
@@ -93,7 +99,7 @@ object Hashes {
       entry = entry.next
       (curr.key, curr.value)
     } else {
-      advance()
+      moveForward()
       nextAt
     }
 
