98c23cbc30de56b9fe2228fe945c71687772daf8
Aleksandar Prokopec <axel22@gmail.com>
2013-06-20 17:52:39 +0200

    Additional test.

--- src/test/scala/scala/collection/parallel/scalatest/TreeStealerTest.scala
+++ src/test/scala/scala/collection/parallel/scalatest/TreeStealerTest.scala
@@ -18,12 +18,40 @@ class TreeStealerTest extends FunSuite {
     hs
   }
 
-  test("HashTrieStealer.advance") {
+  def printHashSet[T](hs: immutable.HashSet[T], indent: Int = 0) {
+    import immutable.HashSet._
+    hs match {
+      case t: HashTrieSet[_] =>
+        println("%sTrie\n".format(" " * indent))
+        for (h <- t.elems) printHashSet(h, indent + 2)
+      case t: HashSet1[_] =>
+        println("%s%s".format(" " * indent, t.iterator.next()))
+    }
+  }
+
+  test("HashTrieStealer.advance(1)") {
     val hs = createHashSet(1)
     val stealer = new workstealing.Trees.HashTrieSetStealer(hs)
     stealer.rootInit()
     assert(stealer.advance(1) == 1)
+    assert(stealer.hasNext)
     assert(stealer.next() == 0)
+    assert(!stealer.hasNext)
+    assert(stealer.advance(1) == -1)
+  }
+
+  test("HashTrieStealer.advance(2)") {
+    val hs = createHashSet(2)
+    val stealer = new workstealing.Trees.HashTrieSetStealer(hs)
+    stealer.rootInit()
+    assert(stealer.advance(1) == 1)
+    assert(stealer.hasNext)
+    assert(hs contains stealer.next())
+    assert(!stealer.hasNext)
+    assert(stealer.advance(1) == 1)
+    assert(stealer.hasNext)
+    assert(hs contains stealer.next())
+    assert(!stealer.hasNext)
     assert(stealer.advance(1) == -1)
   }
 
