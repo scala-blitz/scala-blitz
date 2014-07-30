7dccb3979301ff21a6772580dc31f8dc65ad34df
Aleksandar Prokopec <axel22@gmail.com>
2014-07-30 19:39:25 +0200

    Interesting test.

--- src/test/scala/org/scala/optimized/test/scalameter/ParArrayBench.scala
+++ src/test/scala/org/scala/optimized/test/scalameter/ParArrayBench.scala
@@ -17,6 +17,9 @@ class ParArrayBench extends OnlineRegressionReport with Serializable with ParArr
   val small = 1000000
   val large = 10000000
 
+  @transient
+  lazy val customTasks = new scala.collection.parallel.ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(8))
+
   /* generators */
 
   val opts = Context(
@@ -29,10 +32,10 @@ class ParArrayBench extends OnlineRegressionReport with Serializable with ParArr
     reports.regression.noiseMagnitude -> 0.15)
 
   val oldopts = Context(
-    exec.minWarmupRuns -> 2,
-    exec.maxWarmupRuns -> 4,
-    exec.benchRuns -> 4,
-    exec.independentSamples -> 1,
+    exec.minWarmupRuns -> 10,
+    exec.maxWarmupRuns -> 20,
+    exec.benchRuns -> 20,
+    exec.independentSamples -> 2,
     reports.regression.noiseMagnitude -> 0.75)
 
   /* benchmarks */
@@ -46,131 +49,135 @@ class ParArrayBench extends OnlineRegressionReport with Serializable with ParArr
 
   performance of "Par[Array]" config (opts) in {
 
-    measure method "NonContendedVolatileReads" config (opts) in {
-      def arraysInitV(from: Int) =
-	for (size <- sizes(from)) yield (0 until size).map(i=>new VolatileContainer).toArray
-      def arraysInitNV(from: Int) =
-	for (size <- sizes(from)) yield (0 until size).map(i=>new NonVolatileContainer).toArray
+//     measure method "NonContendedVolatileReads" config (opts) in {
+//       def arraysInitV(from: Int) =
+// 	for (size <- sizes(from)) yield (0 until size).map(i=>new VolatileContainer).toArray
+//       def arraysInitNV(from: Int) =
+// 	for (size <- sizes(from)) yield (0 until size).map(i=>new NonVolatileContainer).toArray
 
-/*      using(arraysInitV(small)) curve ("volatile") in { x => (1 to 100).map{a: Int =>{ var i = 0
-	val to = x.size
-	var acc = 0
-	while(i<to) {acc = acc + x(i).m
-	  i = i + 1}
-	acc}
-      }
-      }*/
-      using(arraysInitNV(small)) curve ("nonVolatile") in { x => (1 to 100).map{a: Int =>{ var i = 0
-	val to = x.size
-	var acc = 0
-	while(i<to) {acc = acc + x(i).m
-	  i = i + 1}
-	acc}
-      }
-      }
+// /*      using(arraysInitV(small)) curve ("volatile") in { x => (1 to 100).map{a: Int =>{ var i = 0
+// 	val to = x.size
+// 	var acc = 0
+// 	while(i<to) {acc = acc + x(i).m
+// 	  i = i + 1}
+// 	acc}
+//       }
+//       }*/
+//       using(arraysInitNV(small)) curve ("nonVolatile") in { x => (1 to 100).map{a: Int =>{ var i = 0
+// 	val to = x.size
+// 	var acc = 0
+// 	while(i<to) {acc = acc + x(i).m
+// 	  i = i + 1}
+// 	acc}
+//       }
+//       }
 
-    }
-    measure method "Boxing" config (opts) in {
-      using(arrays(large)) curve ("Sequential") in { x => noboxing(x)(_ + _) }
-      using(arrays(large)) curve ("SequentialBoxedOp") in { x => boxing(x)(_ + _) }
-      using(arrays(large)) curve ("SequentialSpecialized") in { x => boxingSpec(x)(_ + _) }
-      using(arraysBoxed(small)) curve ("SequentialBoxedOpBoxedData") in { x => boxingSpec(x)(_ + _) }
-    }
+//     }
+//     measure method "Boxing" config (opts) in {
+//       using(arrays(large)) curve ("Sequential") in { x => noboxing(x)(_ + _) }
+//       using(arrays(large)) curve ("SequentialBoxedOp") in { x => boxing(x)(_ + _) }
+//       using(arrays(large)) curve ("SequentialSpecialized") in { x => boxingSpec(x)(_ + _) }
+//       using(arraysBoxed(small)) curve ("SequentialBoxedOpBoxedData") in { x => boxingSpec(x)(_ + _) }
+//     }
 
-    measure method "Dispatch" config (opts) in {
-      val helpers = new Helpers
-      import helpers._
+//     measure method "Dispatch" config (opts) in {
+//       val helpers = new Helpers
+//       import helpers._
 
-      var j = 0
-      var methodId = 0
+//       var j = 0
+//       var methodId = 0
 
-      using(arrays(large)) curve ("Dynamic") in { arr =>
-        val method = methodId % 2
-        methodId = methodId + 1
+//       using(arrays(large)) curve ("Dynamic") in { arr =>
+//         val method = methodId % 2
+//         methodId = methodId + 1
 
-        val inc: Int = if (method == 1) sum1(arr)(_ + _ + 3)
-        else sum1(arr)(_ + _ + 4)
-        j = j + inc
-      }
-      using(arrays(large)) curve ("Static") in { arr =>
-        sum2(arr)
-      }
-    }
+//         val inc: Int = if (method == 1) sum1(arr)(_ + _ + 3)
+//         else sum1(arr)(_ + _ + 4)
+//         j = j + inc
+//       }
+//       using(arrays(large)) curve ("Static") in { arr =>
+//         sum2(arr)
+//       }
+//     }
 
     measure method "reduce" in {
-      using(arrays(large)) curve ("Sequential") in reduceSequential
-      using(withSchedulers(arrays(large))) curve ("Par") in { t => reduceParallel(t._1)(t._2) }
+      // using(arrays(large)) curve ("Sequential") in reduceSequential
+      // using(withSchedulers(arrays(large))) curve ("Par") in { t => reduceParallel(t._1)(t._2) }
       performance of "old" config (oldopts) in {
-        using(arrays(small)) curve ("ParArray") in { _.par.reduce(_ + _) }
-      }
-    }
-
-    measure method "mapReduce" in {
-      using(arrays(large)) curve ("Sequential") in mapReduceSequential
-      using(withSchedulers(arrays(large))) curve ("Par") in { t => mapReduceParallel(t._1)(t._2) }
-    }
-
-    measure method "aggregate" in {
-      using(arrays(small)) curve ("Sequential") in aggregateSequential
-      using(withSchedulers(arrays(small))) curve ("Par") in { t => aggregateParallel(t._1)(t._2) }
-    }
-
-    measure method "find(sin)" in {
-      using(arrays(tiny)) curve ("Sequential") in findSinSequential
-      using(withSchedulers(arrays(tiny))) curve ("Par") in { t => findSinParallel(t._1)(t._2) }
-    }
-
-    measure method "map(sqrt)" in {
-      using(arrays(small)) curve ("Sequential") in mapSqrtSequential
-      using(withSchedulers(arrays(small))) curve ("Par") in { t => mapSqrtParallel(t._1)(t._2) }
-    }
-
-    measure method "filter(mod3)" config (
-      exec.minWarmupRuns -> 80,
-      exec.maxWarmupRuns -> 160) in {
-        using(arrays(small)) curve ("Sequential") in filterMod3Sequential
-        using(withSchedulers(arrays(small))) curve ("Par") in { t => filterMod3Parallel(t._1)(t._2) }
-        performance of "old" config (oldopts) in {
-          using(arrays(tiny)) curve ("ParArray") in { _.par.filter(_ % 3 == 0) }
+        using(arrays(small).map(_.to[collection.mutable.HashSet])) curve ("ParArray") in { a =>
+          val pa = a.par
+          pa.tasksupport = customTasks
+          pa.reduce(_ + _)
         }
       }
-
-    measure method "filter(cos)" in {
-      using(arrays(tiny)) curve ("Sequential") in filterCosSequential
-      using(withSchedulers(arrays(tiny))) curve ("Par") in { t => filterCosParallel(t._1)(t._2) }
     }
 
-    measure method "flatMap" in {
-      using(arrays(small)) curve ("Sequential") in flatMapSequential
-      using(withSchedulers(arrays(small))) curve ("Par") in { t => flatMapParallel(t._1)(t._2) }
-    }
+    // measure method "mapReduce" in {
+    //   using(arrays(large)) curve ("Sequential") in mapReduceSequential
+    //   using(withSchedulers(arrays(large))) curve ("Par") in { t => mapReduceParallel(t._1)(t._2) }
+    // }
 
-    performance of "derivative" in {
-      measure method "fold(product)" in {
-        using(arrays(small)) curve ("Sequential") in foldProductSequential
-        using(withSchedulers(arrays(small))) curve ("Par") in { t => foldProductParallel(t._1)(t._2) }
-      }
+    // measure method "aggregate" in {
+    //   using(arrays(small)) curve ("Sequential") in aggregateSequential
+    //   using(withSchedulers(arrays(small))) curve ("Par") in { t => aggregateParallel(t._1)(t._2) }
+    // }
 
-      measure method "foreach" in {
-        using(arrays(small)) curve ("Sequential") in foreachSequential
-        using(withSchedulers(arrays(small))) curve ("Par") in { t => foreachParallel(t._1)(t._2) }
-      }
+    // measure method "find(sin)" in {
+    //   using(arrays(tiny)) curve ("Sequential") in findSinSequential
+    //   using(withSchedulers(arrays(tiny))) curve ("Par") in { t => findSinParallel(t._1)(t._2) }
+    // }
 
-      measure method ("sum") in {
-        using(arrays(small)) curve ("Sequential") in sumSequential
-        using(withSchedulers(arrays(small))) curve ("Par") in { t => sumParallel(t._1)(t._2) }
-      }
+    // measure method "map(sqrt)" in {
+    //   using(arrays(small)) curve ("Sequential") in mapSqrtSequential
+    //   using(withSchedulers(arrays(small))) curve ("Par") in { t => mapSqrtParallel(t._1)(t._2) }
+    // }
 
-      measure method ("product") in {
-        using(arrays(small)) curve ("Sequential") in productSequential
-        using(withSchedulers(arrays(small))) curve ("Par") in { t => productParallel(t._1)(t._2) }
-      }
+    // measure method "filter(mod3)" config (
+    //   exec.minWarmupRuns -> 80,
+    //   exec.maxWarmupRuns -> 160) in {
+    //     using(arrays(small)) curve ("Sequential") in filterMod3Sequential
+    //     using(withSchedulers(arrays(small))) curve ("Par") in { t => filterMod3Parallel(t._1)(t._2) }
+    //     performance of "old" config (oldopts) in {
+    //       using(arrays(tiny)) curve ("ParArray") in { _.par.filter(_ % 3 == 0) }
+    //     }
+    //   }
 
-      measure method ("count(squareMod3)") in {
-        using(arrays(small)) curve ("Sequential") in countSquareMod3Sequential
-        using(withSchedulers(arrays(small))) curve ("Par") in { t => countSquareMod3Parallel(t._1)(t._2) }
-      }
-    }
+    // measure method "filter(cos)" in {
+    //   using(arrays(tiny)) curve ("Sequential") in filterCosSequential
+    //   using(withSchedulers(arrays(tiny))) curve ("Par") in { t => filterCosParallel(t._1)(t._2) }
+    // }
+
+    // measure method "flatMap" in {
+    //   using(arrays(small)) curve ("Sequential") in flatMapSequential
+    //   using(withSchedulers(arrays(small))) curve ("Par") in { t => flatMapParallel(t._1)(t._2) }
+    // }
+
+    // performance of "derivative" in {
+    //   measure method "fold(product)" in {
+    //     using(arrays(small)) curve ("Sequential") in foldProductSequential
+    //     using(withSchedulers(arrays(small))) curve ("Par") in { t => foldProductParallel(t._1)(t._2) }
+    //   }
+
+    //   measure method "foreach" in {
+    //     using(arrays(small)) curve ("Sequential") in foreachSequential
+    //     using(withSchedulers(arrays(small))) curve ("Par") in { t => foreachParallel(t._1)(t._2) }
+    //   }
+
+    //   measure method ("sum") in {
+    //     using(arrays(small)) curve ("Sequential") in sumSequential
+    //     using(withSchedulers(arrays(small))) curve ("Par") in { t => sumParallel(t._1)(t._2) }
+    //   }
+
+    //   measure method ("product") in {
+    //     using(arrays(small)) curve ("Sequential") in productSequential
+    //     using(withSchedulers(arrays(small))) curve ("Par") in { t => productParallel(t._1)(t._2) }
+    //   }
+
+    //   measure method ("count(squareMod3)") in {
+    //     using(arrays(small)) curve ("Sequential") in countSquareMod3Sequential
+    //     using(withSchedulers(arrays(small))) curve ("Par") in { t => countSquareMod3Parallel(t._1)(t._2) }
+    //   }
+    // }
 
   }
 
